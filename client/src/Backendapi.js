4dd525939e0b78f2b998ac1be1202f95f137d1ab
Vasikaran <vasikaran@p2fsemi.in>
2023-06-08 12:59:42 +0530

    aws dep3

--- client/src/Backendapi.js
+++ client/src/Backendapi.js
@@ -1,10 +1,10 @@
 
 const Backendapi = {
-    REACT_APP_BACKEND_API_URL:"http://65.0.135.17",
+    REACT_APP_BACKEND_API_URL:"http://13.232.25.226",
     REACT_APP_SuperUser_EMAIL:"areesh.kumar@p2fsemi.com"// Initial value as an empty string
     
     
  }
 export default Backendapi
 
-//http://65.0.135.17 bhavya@p2fsemi.com vasikaran@p2fsemi.in areesh.kumar@p2fsemi.com
+//http://13.232.25.226 / bhavya@p2fsemi.com/ vasikaran@p2fsemi.in/ areesh.kumar@p2fsemi.com 
